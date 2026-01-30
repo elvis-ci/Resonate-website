e08ce5cbfd3da7a5e03b22e1fc41538cd34aaf4e
Elvis Izuegbu <izuegbuelvis@gmail.com>
2026-01-30 10:56:44 +0100

    helper file for useWorkspaceLocations composable

---
+++ src/utils/workspaceTypeMap.js
@@ -0,0 +1,8 @@
+// Map display types to DB types
+export const workspaceTypeMap = {
+  'Shared Workspace': 'shared_workspace',
+  'Private Office Suite': 'private_office_suite',
+  'Team Collaboration Room': 'team_collaboration_room',
+  'Executive Conference Room': 'executive_conference_room',
+  'Event & Seminar Hall': 'event_seminar_hall',
+}
